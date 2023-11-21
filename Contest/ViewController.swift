37ec38d1349b64fb50d921806b8716fadd279148
Austin Dobberfuhl <AUSTIN.DOBBERFUHL4597@STU.MTEC.EDU>
2023-11-21 09:30:14 -0700

    Initial Commit

---
+++ Contest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Contest
+//
+//  Created by Austin Dobberfuhl on 11/21/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
