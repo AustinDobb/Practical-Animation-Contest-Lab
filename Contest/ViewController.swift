83b2cb7dcdbbb3fdd34d37954be87d7f373b1188
Austin Dobberfuhl <AUSTIN.DOBBERFUHL4597@STU.MTEC.EDU>
2023-11-28 09:20:14 -0700

    This project is finshed

--- Contest/ViewController.swift
+++ Contest/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    
+    @IBAction func submitButtonTapped(_ sender: UIButton) {
+        
+        if emailTextField.text == "" {
+            UIView.animate(withDuration: 0.5) {
+                let translateTransform = CGAffineTransform(translationX: 15, y: 0)
+                self.emailTextField.transform = translateTransform
+            } completion: { _ in
+                UIView.animate(withDuration: 0.5) {
+                    self.emailTextField.transform = .identity
+                }
+            }
+        } else {
+            performSegue(withIdentifier: "contestEntered", sender: sender)
+        }
+    }
 }
 
